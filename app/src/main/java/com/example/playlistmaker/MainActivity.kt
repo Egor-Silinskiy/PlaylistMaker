8674b0a074ad7b54b060988cc413bf0ae9402e8e
Egor-Silinskiy <egorkakegotam@gmail.com>
2025-11-28 16:50:51 +0300

    Sprint 8 task 2

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +15,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+
+        val button1 = findViewById<Button>(R.id.button1)
+
+        val imageClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажали на поиск!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        button1.setOnClickListener(imageClickListener)
+
+        val button2 = findViewById<Button>(R.id.button2)
+
+        button2.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на медиатеку!", Toast.LENGTH_SHORT).show()
+        }
+
+        val button3 = findViewById<Button>(R.id.button3)
+
+        button3.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на настройки!", Toast.LENGTH_SHORT).show()
+        }
 
     }
 }
