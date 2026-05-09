671bf151d07134f58fe8ea058986a41ad8b21e01
Egor-Silinskiy <egorkakegotam@gmail.com>
2026-05-09 22:59:17 +0300

    Sprint 10
    1. Создан класс Track.
    2. В SearchActivity.kt создан список с объектами Track.
    3. Создан TrackViewHolder.kt и TrackAdapter.kt.
    4. Подключена библиотека Glide, дано разрешение на доступ в интернет в манифесте.
    5. Добавлен placeholder.
    6. Добавлена тёмная тема.

--- app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
@@ -1,21 +1,31 @@
 package com.example.playlistmaker
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
 class TrackViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
+    private val artwork: ImageView = itemView.findViewById(R.id.artworkUrl)
     private val trackName: TextView = itemView.findViewById(R.id.trackName)
     private val artistName: TextView = itemView.findViewById(R.id.artistName)
     private val trackTime: TextView = itemView.findViewById(R.id.trackTime)
-    private val artworkUrl100: TextView = itemView.findViewById(R.id.artworkUrl100)
 
     fun bind(model: Track) {
+        Glide.with(itemView)
+            .load(model.artworkUrl)
+            .placeholder(R.drawable.placeholder)
+            .error(R.drawable.placeholder)
+            .centerCrop()
+            .transform(RoundedCorners(2))
+            .into(artwork)
+
         trackName.text = model.trackName
-        artistName.text = model.trackName
-        trackTime.text = model.trackName
-        artworkUrl100.text = model.trackName
+        artistName.text = model.artistName
+        trackTime.text = model.trackTime
     }
 
 }
